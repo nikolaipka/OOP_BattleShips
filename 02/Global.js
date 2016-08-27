c8a83c7c04e3647618e70a64dfacf9ea0c4ad0de
Nikolay Liubomirov <jack_niki99@abv.bg>
2016-08-27 23:55:22 +0300

    scratches

--- 02/Global.js
+++
@@ -1,1 +0,0 @@
-var _priceForAMinute = 10;
---
+++ assets/js/Demo.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+	var grid = new Grid(20, 20);
+	grid.CreateGrid();
+	grid.Visualize();
+}, false)
